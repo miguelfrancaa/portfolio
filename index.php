bf8973068fd061ac63e584fd771c6a404323ba9f
miguelfrancaa <miguelcrfranca@gmail.com>
2023-07-11 19:53:17 +0100

    FIRST COMMIT

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+
+	$url_parts = explode("/", $_SERVER["REQUEST_URI"]);
+
+	define("ENV", parse_ini_file(".env"));
+
+	$controller = "home";
+
+	$allowed_controllers = [
+
+	];
+
+	if(!empty($url_parts[1])) {
+		$controller = $url_parts[1];
+	}
+
+	if(!empty($url_parts[2])) {
+		$id = $url_parts[2];
+	}
+
+	 if( !in_array($controller, $allowed_controllers) ) {
+    http_response_code(404);
+    die("NOT FOUND!");
+} 
+
+
+	require("controllers/" . $controller . ".php");
+	
+
+?>
